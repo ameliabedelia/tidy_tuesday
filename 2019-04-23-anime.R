8dba42c3c0d3c0cf9e66aecdb51114ab08835385
Amelia Bedelia <44848124+ameliabedelia@users.noreply.github.com>
2019-05-01 19:26:30 +0200

    Update 2019-04-23-anime.R

--- 2019-04-23-anime.R
+++ 2019-04-23-anime.R
@@ -1,11 +1,10 @@
 library(tidyverse)
-library(viridis)
 library(ggthemes)
 
 tidy_anime <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-04-23/tidy_anime.csv")
 
 score_summary <- tidy_anime %>%
-     filter(!is.na(studio)) %>%  
+     filter(!is.na(studio)) %>% 
      group_by(studio) %>%
      summarise(
           n = n(),
@@ -15,19 +14,19 @@ score_summary <- tidy_anime %>%
      ungroup()
 
 plot <- ggplot(score_summary, aes(n, score_mean)) +
-     geom_point(aes(color = scored_by_mean, size = scored_by_mean), alpha = 0.7,
+     geom_point(aes(size = scored_by_mean), alpha = 0.7,
                 show.legend = FALSE) +
-     geom_smooth(se = FALSE, color = "darkgray") +
+     geom_smooth(se = FALSE, color = "dimgray") +
      scale_x_log10() +
      scale_color_viridis(option = "A") +
      labs(title = "Do Bigger Studios Make Higher Rated Anime?",
-          subtitle = "Point size and color correlates with number of user ratings",
+          subtitle = "Point size correlates with number of user ratings",
           x = "Number of Titles from Studio",
           y = "Average Rating",
           caption = "Data: MyAnimeList \nVisualization: @frau_dr_barber") +
      theme_solarized() +
      theme(plot.title = element_text(color = "black"),
-          plot.subtitle = element_text(size = 10),
+          plot.subtitle = element_text(size = 9),
           plot.caption = element_text(size = 7))
 
 ggsave("studio_rating.png", height = 4, width = 5, units = "in")
