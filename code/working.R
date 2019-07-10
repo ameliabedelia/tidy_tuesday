82c12d32fbaf59d80aa3f79dc18e896e39eff258
Amelia Bedelia <44848124+ameliabedelia@users.noreply.github.com>
2019-07-10 17:30:58 +0200

    Update working.R

--- code/working.R
+++ code/working.R
@@ -33,21 +33,38 @@ wwc_outcomes %>%
     )
   ) %>%
   filter(!is.na(place)) %>%
-  arrange(desc(year), place) %>% 
-  mutate(x1 = row_number()) %>% 
+  #arrange(desc(year), place) %>% 
+  mutate(highlight = if_else(team == "USA", "yes", "no")) %>% 
   ggplot(aes(year, place, group = team)) +
-  geom_line(aes(color = team), size = 2) +
-  geom_point(aes(color = team), size = 4) +
+  geom_line(aes(color = highlight), size = 2) +
+  geom_point(aes(color = highlight), size = 4) +
   geom_point(color = "white", size = 1) +
-  geom_text(data = . %>% filter(year == 1991, place < 5), aes(label = team, x = 1989)) +
+  labs(x = "World Cup",
+       y = "",
+       title = "The US women's team is the winningest team in world cup history",
+       subtitle = "Despite this, they make dramatically less than male players - even though
+they win more games and generate more revenue"
+       ) +
+  #geom_text(data = . %>% filter(year == 1991, place < 5), aes(label = team, x = 1989)) +
   geom_text(data = . %>% filter(year == 2019, place < 5), aes(label = team, x = 2021)) +
   scale_x_continuous(breaks = c(1991, 1995, 1999, 2003, 2007, 2011, 2015, 2019),
                      expand = c(.05, .05)) +
-  scale_y_continuous(trans = "reverse", labels = c("1st", "2nd", "3rd", "4th",
-                                                   "Quarter Finals\n5th-8th")) +
-  scale_color_paletteer_d("pals", "alphabet")
+  scale_y_continuous(trans = "reverse", 
+                     labels = c("1st", "2nd", "3rd", "4th",
+                                "5th-8th\n(Quarter Finals)")) +
+  scale_color_manual(values = c("gray", "red")) +
+  #coord_cartesian(ylim = c(1:5))
+  theme_minimal() +
+  theme(legend.position = "none")
+
 
 palettes_d_names %>% 
   as_tibble() %>% 
   filter(type == "qualitative") %>% 
   arrange(desc(length))
+
+wwc_outcomes %>% 
+  group_by(year, round) %>% 
+  count() %>%
+  arrange(year, n) %>% 
+  view()
