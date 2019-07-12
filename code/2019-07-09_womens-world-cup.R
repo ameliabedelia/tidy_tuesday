cade37a1ec3655ebd556929a81af328abc90ebe7
Amelia Bedelia <44848124+ameliabedelia@users.noreply.github.com>
2019-07-12 19:51:49 +0200

    Update 2019-07-09_womens-world-cup.R

--- code/2019-07-09_womens-world-cup.R
+++ code/2019-07-09_womens-world-cup.R
@@ -1,10 +1,13 @@
 library(tidyverse)
+library(paletteer)
 
 wwc_outcomes <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-09/wwc_outcomes.csv")
 logo <- png::readPNG("uswnt.png")
 logo <- grid::rasterGrob(logo, interpolate = TRUE) 
 usa_blue <- "#1D2642"
 usa_red <- "#BB222E"
+grey_palette <- paletteer_dynamic("cartography", "grey.pal", 17, direction = 1)
+grey_palette <- grey_palette[1:11]
 
 wwc_outcomes %>%
      mutate(
@@ -20,13 +23,13 @@ wwc_outcomes %>%
      mutate(highlight = if_else(team == "USA", "yes", "no")) %>% 
      ggplot(aes(year, place, group = team)) +
      annotation_custom(logo, xmin = 2003, xmax = 2007, ymin = 3.95, ymax = 4.95) +
-     geom_line(aes(color = highlight), size = 1) +
-     geom_point(aes(color = highlight), size = 6) +
+     geom_line(aes(color = team), size = 1) +
+     geom_point(aes(color = team), size = 6) +
      labs(x = "World Cup",
           y = "",
           title = "\nThe US Women's Team is the Winningest Team\nin World Cup History",
           subtitle = "The women's team is more successful and brings in more revenue than the men's team, 
-yet they are paid significantly less - around 38% of what the men make.",
+          yet they are paid significantly less - around 38% of what the men make.",
           caption = "Source: data.world
           Visualization: Frau_Dr_Barber"
      ) +
@@ -36,7 +39,7 @@ yet they are paid significantly less - around 38% of what the men make.",
                         expand = c(.05, .05)) +
      scale_y_continuous(labels = c("5th-8th\n(Quarter Finals)",
                                    "4th", "3rd", "2nd", "1st")) +
-     scale_color_manual(values = c("gray", usa_red)) +
+     scale_color_manual(values = c(sample(grey_palette, 17, replace = TRUE), usa_red)) +
      theme_minimal() +
      theme(legend.position = "none",
            panel.grid.major.y = element_blank(),
